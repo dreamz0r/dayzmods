73ef62b7edeee0c2c00131e7b9291852ff524cd3
dreamz0r <110413248+dreamz0r@users.noreply.github.com>
2025-03-29 20:34:22 +0200

    Add files via upload

---
+++ config.cpp
@@ -0,0 +1,134 @@
+class CfgPatches
+{
+	class DZ_Gear_Containers
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data"
+		};
+	};
+};
+class CfgVehicles
+{
+	class Container_Base;
+	class ScopeCase: Container_Base
+	{
+		scope=2;
+		displayName="Scope Case";
+		descriptionShort="Scope Case";
+		model="\dz\gear\containers\Protector_Case.p3d";
+		rotationFlags=17;
+		weight=980;
+		itemSize[]={3,4};
+		itemsCargoSize[]={1,1};
+		canBeDigged=1;
+		isMeleeWeapon=1;
+		allowOwnedCargoManipulation=1;
+		randomQuantity=2;
+		repairableWithKits[]={8};
+		repairCosts[]={25};
+		attachments[]=
+		{
+			"BatteryD",
+			"weaponOptics",
+			"WeaponOptics",
+			"pistolOptics",
+			"weaponOpticsMosin",
+			"weaponOpticsAK",
+			"weaponOpticsLRS",
+			"weaponOpticsHunting",
+			"weaponOpticsCrossbow"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=750;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"DZ\gear\containers\data\Protector_Case.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"DZ\gear\containers\data\Protector_Case.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"DZ\gear\containers\data\Protector_Case_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"DZ\gear\containers\data\Protector_Case_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"DZ\gear\containers\data\Protector_Case_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+			class GlobalArmor
+			{
+				class FragGrenade
+				{
+					class Health
+					{
+						damage=8;
+					};
+					class Blood
+					{
+						damage=8;
+					};
+					class Shock
+					{
+						damage=8;
+					};
+				};
+			};
+		};
+		soundImpactType="plastic";
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickUpItem
+				{
+					soundSet="smallprotectorcase_pickup_SoundSet";
+					id=797;
+				};
+				class drop
+				{
+					soundset="smallprotectorcase_drop_SoundSet";
+					id=898;
+				};
+			};
+		};
+	};
+};
